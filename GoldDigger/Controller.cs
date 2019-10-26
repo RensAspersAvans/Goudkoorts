08aeeb5e5fa16a3dc062081433a66fe06ff2cc34
jvanspijk <jcrspijk@avans.nl>
2019-10-26 15:14:07 +0200

    GameOverView + Controller
    GameOverView gemaakt, logica gemaakt voor begin en einde spel

--- GoldDigger/Controller.cs
+++ GoldDigger/Controller.cs
@@ -1,17 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Timers;
 
 namespace GoldDigger
 {
     public class Controller
     {
-        public GameModel GameModel
+        private static System.Timers.Timer timer;
+
+        public Controller()
+        {
+            //Maak een timer aan
+            
+            timer = new System.Timers.Timer(1000); //1000 ms = 1 tick per seconde
+
+            timer.Elapsed += new ElapsedEventHandler((source, e) => Tick());
+            timer.AutoReset = true;
+            
+        }
+        public GameModel game
         {
             get => default;
             set
             {
             }
         }
+
+        public GameView gameView
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public void Run()
+        {
+            timer.Enabled = true;           
+        }
+
+        private void Stop()
+        {
+            timer.Enabled = false;
+            GameOverView endScreen = new GameOverView();
+            endScreen.Show(-1);
+            //endScreen.Show(game.GetScore());
+        }
+
+        public void Tick()
+        {
+            //game.update();
+            
+            if(game.GameOver())
+            {
+                Stop();
+            }
+
+            
+        }
+
+        
     }
 }
---
+++ GoldDigger/GameOverView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GoldDigger
+{
+    class GameOverView
+    {
+        public GameOverView()
+        {
+            Console.Clear();                       
+        }
+
+        public void Show(int score)
+        {
+            ShowGameOverText();
+            ShowScore(score);
+        }
+
+        public void ShowGameOverText()
+        {
+            Console.WriteLine("G A M E  O V E R");
+            Console.WriteLine();
+        }
+
+        public void ShowScore(int score)
+        {
+            Console.WriteLine("Score:  " + score);
+        }
+    }
+
+
+    
+}
