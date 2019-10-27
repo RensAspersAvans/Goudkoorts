60de32d78052c1b0ba2eb75af25929482c1ef346
RensAspersAvans <rca.asper@student.avans.nl>
2019-10-27 13:16:05 +0100

    mappen

---
+++ GoldDigger/Processing/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GoldDigger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+                Console.WriteLine("     1 - 5:   Verander switch 1 - 5 van richting");
+                Console.WriteLine("     S:       Stop met spelen");
+
+            new Controller().Run();
+            
+        }
+    }
+}
